d6970158cdb2660170661bb290cacf3299010a5d
Aidan Cunniffe <acunniffe@gmail.com>
2018-04-21 14:44:50 -0400

    Added registry

--- src/registry/Config.js
+++ src/registry/Config.js
@@ -1,2 +1,2 @@
-export const registryHost = 'https://cdmfgxf5e1.execute-api.us-east-2.amazonaws.com/production'
+export const registryHost = 'https://registry.opticdev.com'
 export const hardcodedRegsitry = 'registry.opticdev.com'
